68c5b1404a87af04f09e7329267adc57be8602dc
Sheldon Demario <sheldon.demario@openbossa.org>
2011-06-16 11:49:16 -0300

    Adding Threshold label

--- monitor.cpp
+++ monitor.cpp
@@ -1,17 +1,29 @@
 #include <QDebug>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
 #include "monitor.h"
 
 Monitor::Monitor(QWidget *parent, Qt::WindowFlags f)
 	: QWidget(parent, f)
 {
-	this->threshold = new QSlider(Qt::Horizontal);
+	QHBoxLayout *sliderBox;
+	QLabel *label;
+
+	sliderBox = new QHBoxLayout();
+	slidersBox = new QVBoxLayout(this);
+
+	label = new QLabel("Threshold");
+	threshold = new QSlider(Qt::Horizontal);
 	threshold->setMaximum(2);
 	connect(threshold, SIGNAL(valueChanged(int)),
 		this, SIGNAL(thresholdChanged(int)));
 
-	this->slidersBox = new QVBoxLayout(this);
-	this->slidersBox->addWidget(threshold);
+	sliderBox->addWidget(label);
+	sliderBox->addSpacing(5);
+	sliderBox->addWidget(threshold);
+
+	slidersBox->addLayout(sliderBox);
 
 	setLayout(slidersBox);
 }
