fd2128e0221c6bb72b9463544d101a3c0e90a724
Sheldon Demario <sheldon.demario@openbossa.org>
2011-08-11 18:19:00 -0300

    Add License files and headers

--- monitor.h
+++ monitor.h
@@ -1,3 +1,24 @@
+/* monitor.h -- This file declares the Monitor class which encapsulates all
+   needed d-bus calls used in this program. */
+
+/* Copyright (C) 2011 Instituto Nokia de Tecnologia
+
+   Author: Sheldon Almeida Demario <sheldon.demario@openbossa.org>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <QObject>
 #include <QDBusVariant>
 #include <QList>
--- types.h
+++ types.h
@@ -1,5 +1,7 @@
 /* Copyright (C) 2011 Instituto Nokia de Tecnologia
-   Copyright (C) 2011 Sheldon Almeida Demario
+
+   Author: Sheldon Almeida Demario <sheldon.demario@openbossa.org>
+   Author: Vinicius Costa Gomes <vinicius.gomes@openbossa.org>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
